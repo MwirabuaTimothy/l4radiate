65de8ea817e00818b469069e618722c2cd5d7035
Timothy Mwirabua <techytimo@gmail.com>
2015-08-14 08:54:35 +0300

    security

--- serving file changes/database.php
+++ serving file changes/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'thedmaea_bookcheetah',
 			'username'  => 'thedmaea_tim',
-			'password'  => 'timo4257',
+			'password'  => 'password',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
